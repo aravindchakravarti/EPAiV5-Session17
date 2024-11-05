1f160a6582e164daaa41600d67d59ed4e6b15b91
Aravind D Chakravarti <aravinddcsadguru@gmail.com>
2024-11-05 06:21:33 +0530

    All tests are passing

--- student_merge.py
+++ student_merge.py
@@ -1,12 +1,40 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
-def merge_with_defaultdict(*arg_dicts):
+def merge_with_defaultdict(*dicts: dict):
+    '''
+    Merges multiple dictionaries by summing values with the same keys.
+    Uses defaultdict to handle missing keys.
+
+    Args:
+    ----
+    dicts: Any number of dictionaries to merge.
+
+    Returns:
+    -------
+    merge_dict: A merged dictionary with summed values.
+    '''
     merge_dict = defaultdict(int)
-    for args in arg_dicts:
-        for key, value in args.items():
-            merge_dict[key] = merge_dict[key] + value
+    for d in dicts:
+        for key, value in d.items():
+            merge_dict[key] += value
     
-    return merge_dict
+    return dict(merge_dict)
 
-def merge_with_counter():
-    pass
+def merge_with_counter(*dicts):
+    '''
+    Merges multiple dictionaries by summing values with the same keys.
+    Uses Counter to handle missing keys and merge dictionaries efficiently.
+
+    Args:
+    ----
+    dicts: Any number of dictionaries to merge.
+
+    Returns:
+    -------
+    merge_dict: A merged dictionary with summed values.
+    '''
+    merge_dict = Counter()
+    for d in dicts:
+        merge_dict.update(d)
+
+    return dict(merge_dict)
